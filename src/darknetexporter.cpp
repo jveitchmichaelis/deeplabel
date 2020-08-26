ee8ed5aecf2da76cbe8034e11092ad71fa94fb3b
Josh Veitch-Michaelis <j.veitchmichaelis+github@gmail.com>
2020-08-26 10:34:51 +0000

    Update darknetexporter.cpp

--- src/darknetexporter.cpp
+++ src/darknetexporter.cpp
@@ -48,10 +48,10 @@ void DarknetExporter::writeLabels(const cv::Mat &image, const QString label_file
                 continue;
             }
 
-            double x = std::clamp(static_cast<double>(label.rect.center().x())/image.cols, 0, 0.999);
-            double y = std::clamp(static_cast<double>(label.rect.center().y())/image.rows, 0, 0.999);
-            double width = std::clamp(static_cast<double>(label.rect.width())/image.cols, 0, 0.999);
-            double height = std::clamp(static_cast<double>(label.rect.height())/image.rows, 0, 0.999);
+            double x = std::clamp(static_cast<double>(label.rect.center().x())/image.cols, 0.0, 0.999);
+            double y = std::clamp(static_cast<double>(label.rect.center().y())/image.rows, 0.0, 0.999);
+            double width = std::clamp(static_cast<double>(label.rect.width())/image.cols, 0.0, 0.999);
+            double height = std::clamp(static_cast<double>(label.rect.height())/image.rows, 0.0, 0.999);
 
             text += QString("%1").arg(id_map[label.classname.toLower()]);
             text += QString(" %1").arg(x);
